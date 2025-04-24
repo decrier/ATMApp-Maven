98ecdc8e60615b1748ac25dbab1230a891ac5600
Mykola Havrysh <decrier@ukr.net>
2025-04-24 14:54:47 +0200

    first with Mavon

---
+++ src/main/java/app/ATMApp.java
@@ -0,0 +1,22 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.lang.Override;
+
+public class ATMApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        primaryStage.setTitle("ATM Login");
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
